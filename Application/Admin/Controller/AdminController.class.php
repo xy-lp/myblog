5610e8ddecabb65cdd5b8c4a0b9f50c116748d41
just <lipei@qinhutouzi.com>
2016-03-04 14:23:09 +0800

    修改后台左导航栏

--- Application/Admin/Controller/AdminController.class.php
+++ Application/Admin/Controller/AdminController.class.php
@@ -28,6 +28,25 @@ class AdminController extends BaseController{
      * 后台左导航栏页面
      */
     public function left(){
+        $user=session('username');  //获取保存在会话中的用户名
+        //通过用户名在user表中查找相应数据中的rule(权限)字段
+        $userRole=M('user')->field('us_role_id')->where(array('us_username'=>$user))->find();
+        //通过在获取到的rule在权限表中查找相应的权限
+        $rules=M('role')->field('re_rules')->where(array('re_id'=>$userRole['us_role_id']))->find();
+        $ru_array=explode(',',$rules['re_rules']);
+
+        $model=M('rule');
+        $map['ru_id']=array('in',$ru_array);
+        $map['ru_status']=array('eq','1');
+        $map['ru_pid']=array('eq','0');
+        $list=$model->order('ru_id asc')->where($map)->select();
+
+        $map['ru_pid']=array('neq','0');
+        $info=$model->order('ru_order asc')->where($map)->select();
+
+        //p($info);
+        $this->assign('list',$list);
+        $this->assign('info',$info);
         $this->display();
     }
 
